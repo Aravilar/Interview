d56b85d932c09bfa60629cd7ad590fb55ef976cf
Benjamin Laczkovics <fekete.laczkovics@gmail.com>
2023-02-06 14:50:27 +0100

    Update next.config.js

--- interview/next.config.js
+++ interview/next.config.js
@@ -4,4 +4,11 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  env: {
+    DB_USER: process.env.DB_USER,
+    DB_HOST: process.env.DB_HOST,
+    DB_NAME: process.env.DB_NAME,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+  },
+}
